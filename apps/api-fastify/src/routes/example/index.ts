242c3731f8e474be84e13be7110dbc500675cf0a
isoppp <prog@d2deck.com>
2024-09-25 00:13:48 +0900

    delete fastify

--- apps/api-fastify/src/routes/example/index.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyPluginAsync } from 'fastify'
-
-const example: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.get('/', async () => 'this is an example')
-}
-
-export default example
